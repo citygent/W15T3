07b975b8d0f4a327cbe367e05f10f543c21e345c
James White <cyberyt@gmail.com>
2015-11-24 19:57:17 +0000

    we can console log some comments

--- problem.js
+++ problem.js
@@ -3,14 +3,14 @@
 var http = require('http')
 var mongodb = require('mongodb')
 var ObjectId = mongodb.ObjectId
-var url = 'mongodb://localhost:27017'
+var url = 'mongodb://localhost:27017/pinipa_exercise_blog'
 var port = 8000
 
 var server = http.createServer(function requestHandler (req, res) {
-  console.log(req.method, req.url)
+  // console.log(req.method, req.url)
 
   if(req.url !== '/comments') {
-    res.writeHead(200, {"Content-Type": "application/json"})
+    res.writeHead(200, {'Content-Type': 'application/json'})
     var json = JSON.stringify({
       Document: "Blank JSON" 
     })
@@ -22,12 +22,10 @@ var server = http.createServer(function requestHandler (req, res) {
 })
 
 mongodb.MongoClient.connect(url, {}, function (err, connection) {
-
-  var collection = connection.collection('comments')
+  connection.collection('comments').find().each(function(err, data){console.log(data)})
 
   server.listen(port, function () {
     console.log('listening on port %d', port)
   })
 })
-
 
