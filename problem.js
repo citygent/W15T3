7a5ba4b3629cbabd0ec3e48eca401f4835115445
James White <cyberyt@gmail.com>
2015-11-29 18:50:17 +0000

    rendering JSON comments

--- problem.js
+++ problem.js
@@ -7,25 +7,36 @@ var url = 'mongodb://localhost:27017/pinipa_exercise_blog'
 var port = 8000
 
 var server = http.createServer(function requestHandler (req, res) {
-  // console.log(req.method, req.url)
+  console.log(req.method, req.url)
 
   if(req.url !== '/comments') {
     res.writeHead(200, {'Content-Type': 'application/json'})
-    var json = JSON.stringify({
-      Document: "Blank JSON" 
-    })
-    res.end(json)
+    var json = {
+      Document: "Blank JSON" // trololol
+    }
+    res.end(JSON.stringify(json))
   } else {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('<html><head><title>Works?</title></head><body>It only fucking works!!</body></html>')
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(result))
   }
 })
 
-mongodb.MongoClient.connect(url, {}, function (err, connection) {
-  connection.collection('comments').find().each(function(err, data){console.log(data)})
+var result;
 
-  server.listen(port, function () {
-    console.log('listening on port %d', port)
-  })
+mongodb.MongoClient.connect(url, {}, function (err, connection) {
+  if (err) throw err;
+
+  connection.collection('comments').find().toArray(function(err, commentData){
+      // var i;
+      // for (i=0; i < data.length; i++) {
+      //   dataArray.push(data[i]);
+      // }
+      // connection.close;
+      result = commentData;
+    })
+  connection.close;
 })
 
+server.listen(port, function (){
+  console.log('listening on port %d', port)
+});
