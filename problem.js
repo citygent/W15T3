3a567f01d02142d75adf776a6c0935d6a4ff8829
James White <cyberyt@gmail.com>
2015-11-30 11:37:21 +0000

    renders JSON to endpoint

--- problem.js
+++ problem.js
@@ -11,10 +11,7 @@ var server = http.createServer(function requestHandler (req, res) {
 
   if(req.url !== '/comments') {
     res.writeHead(200, {'Content-Type': 'application/json'})
-    var json = {
-      Document: "Blank JSON" // trololol
-    }
-    res.end(JSON.stringify(json))
+    res.end(JSON.stringify({}));
   } else {
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end(JSON.stringify(result))
@@ -25,30 +22,33 @@ var result = [];
 var commentArray, userArray;
 
 mongodb.MongoClient.connect(url, {}, function (err, connection) {
-  if (err) throw err;
+  if (err) console.log(err);
   var blog = connection.db('pinipa_exercise_blog');
   blog.collection('comments').find().toArray(function(err, commentData){
+    if (err) result.push(err); // pushing errors to JSON response
     commentArray = commentData;
     // console.log(commentArray)
   });
   var global = connection.db('pinipa_exercise_global');
   global.collection('users').find().toArray(function(err, userData){
+    if (err) result.push(err); // pushing errors to JSON response
     userArray = userData;
 
     for (var i = 0; i < commentArray.length; i++) {
       for (var j = 0; j < userArray.length; j++) {
         if (String(commentArray[i].user) === String(userArray[j]._id)) {
-          var commentObj = {}
-          commentObj.firstName = userArray[j].first_name;
-          commentObj.lastName = userArray[j].last_name;
+          var commentObj = {
+            user: {}
+          }
+          commentObj._id = commentArray[i]._id;
+          commentObj.user.firstName = userArray[j].first_name;
+          commentObj.user.lastName = userArray[j].last_name;
+          commentObj.date = commentArray[i].date;
           commentObj.content = commentArray[i].content;
-          console.log(commentObj)
+          result.push(commentObj)
         }
       };
     };
-
-
-
     // console.log(userArray)
   })
   connection.close;
