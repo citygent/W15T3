86fde5a1a29f15574f8a7d80189c60cef997e6ae
James White <cyberyt@gmail.com>
2015-11-30 11:44:51 +0000

    refactored so will perform get from DB on http GET

--- problem.js
+++ problem.js
@@ -14,53 +14,44 @@ var server = http.createServer(function requestHandler (req, res) {
     res.end(JSON.stringify({}));
   } else {
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(result))
-  }
-})
 
-var result = [];
-var commentArray, userArray;
+    var result = [];
+    var commentArray, userArray;
 
-mongodb.MongoClient.connect(url, {}, function (err, connection) {
-  if (err) console.log(err);
-  var blog = connection.db('pinipa_exercise_blog');
-  blog.collection('comments').find().toArray(function(err, commentData){
-    if (err) result.push(err); // pushing errors to JSON response
-    commentArray = commentData;
-    // console.log(commentArray)
-  });
-  var global = connection.db('pinipa_exercise_global');
-  global.collection('users').find().toArray(function(err, userData){
-    if (err) result.push(err); // pushing errors to JSON response
-    userArray = userData;
+    mongodb.MongoClient.connect(url, {}, function (err, connection) {
+      if (err) console.log(err);
+      var blog = connection.db('pinipa_exercise_blog');
+      blog.collection('comments').find().toArray(function(err, commentData){
+        if (err) result.push(err); // pushing errors to JSON response
+        commentArray = commentData;
+        // console.log(commentArray)
+      });
+      var global = connection.db('pinipa_exercise_global');
+      global.collection('users').find().toArray(function(err, userData){
+        if (err) result.push(err); // pushing errors to JSON response
+        userArray = userData;
 
-    for (var i = 0; i < commentArray.length; i++) {
-      for (var j = 0; j < userArray.length; j++) {
-        if (String(commentArray[i].user) === String(userArray[j]._id)) {
-          var commentObj = {
-            user: {}
-          }
-          commentObj._id = commentArray[i]._id;
-          commentObj.user.firstName = userArray[j].first_name;
-          commentObj.user.lastName = userArray[j].last_name;
-          commentObj.date = commentArray[i].date;
-          commentObj.content = commentArray[i].content;
-          result.push(commentObj)
-        }
-      };
-    };
-    // console.log(userArray)
-  })
-  connection.close;
-})
-
-if (commentArray) {
-  console.log('commentArray!')
-}
-
-if (userArray) {
-  console.log('userArray!')
-}
+        for (var i = 0; i < commentArray.length; i++) {
+          for (var j = 0; j < userArray.length; j++) {
+            if (String(commentArray[i].user) === String(userArray[j]._id)) {
+              var commentObj = {
+                user: {}
+              }
+              commentObj._id = commentArray[i]._id;
+              commentObj.user.firstName = userArray[j].first_name;
+              commentObj.user.lastName = userArray[j].last_name;
+              commentObj.date = commentArray[i].date;
+              commentObj.content = commentArray[i].content;
+              result.push(commentObj)
+            }
+          };
+        };
+        res.end(JSON.stringify(result))
+      })
+      connection.close;
+    })
+  };
+});
 
 server.listen(port, function (){
   console.log('listening on port %d', port)
